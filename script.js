6d924a9480d42bad277866400219d512bdef38db
Kyle <kyleryan480@gmail.com>
2018-02-26 00:34:01 -0700

    grid, +2 methods

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-alert("js connected");
+$(document).ready(function(){
+    $("#btnTwo").click(function(){
+        $("#pTwo").toggle();
+    });
+
+    $("#btnOne").click(function(){
+        $("#pOne").append(" <t>|</t>");
+    });
+});
+
+// $(document).ready(function(){
+    
+
+//     $("#btn2").click(function(){
+//         $("ol").append("<li>Appended item</li>");
+//     });
+// });
