426e27f106c53f2266445f7b2178bb9a06594ad7
Kyle <kyleryan480@gmail.com>
2018-02-28 00:31:08 -0700

    - html, + grammer

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 $(document).ready(function(){
 
-    $("#btnAddClass").click(function(){
+    $("#h5AddClass").click(function(){
         $("#pAddClass").addClass("negative");
     });
 
-    $("#btnAnimate").click(function(){
+    $("#h5Animate").click(function(){
         $("#pAnimate")
-        .animate({ 'right': "+=24"}, 250)
-        .animate({ 'right': "-=48"}, 250)
-        .animate({ 'right': "+=24"}, 250);
+        .animate({ right: "+=24" }, 250)
+        .animate({ right: "-=48"}, 250)
+        .animate({ right: "+=24"}, 250);
     })
    
-    $("#btnAppend").click(function(){
+    $("#h5Append").click(function(){
         $("#pAppend").append(" <t>.</t>");
     });
     
-    $("#btnToggle").click(function(){
+    $("#h5Toggle").click(function(){
         $("#pToggle").toggle();
     });
 });
